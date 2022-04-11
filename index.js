0f59ba209451d884dd85fbdc96fe0f45f52898de
mohamedsahal <mohaansahal@gmail.com>
2022-04-11 10:27:50 +0300

    innit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+var randomNumber1 =Math.floor( Math.random()*6)+1;
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", randomImageSource);
+
+    var randomNumber2 =Math.floor( Math.random()*6)+1;
+    var randomDiceImage2 = "dice"+randomNumber2+".png";
+    var randomImageSource = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImageSource);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = " 🚩 Player 1 Won";
+} else if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Won  🚩";
+}else{
+    document.querySelector("h1").innerHTML = "Drow";
+}
